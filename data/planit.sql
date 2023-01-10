6e93fc49f7bd9bfa45c071efd27f4162a7b07ad5
SoilworkerSupre <grahammonnig@gmail.com>
2023-01-10 18:12:31 -0500

    added journal for Graham, added poll_service folder (to be filled, initial commmit)

--- data/planit.sql
+++ data/planit.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS accounts, polls, poll_options;
 
 CREATE TABLE accounts (
     -- id INTEGER NOT NULL UNIQUE,
@@ -10,46 +10,3 @@ CREATE TABLE accounts (
     username TEXT NOT NULL,
     referrer_id INTEGER REFERENCES accounts("id") ON DELETE CASCADE
 );
-
-
-<<<<<<< HEAD
-INSERT INTO users VALUES
-  (1, 'John', 'Smith', '1780000.jpeg', 'John@gmail.com', 'Jsmith', null),
-  (2, 'Dave', 'Jones', '178030988.jpeg', 'Dave@gmail.com', 'Djones', null),
-  (3, 'Patrick', 'Lacquer', '17800.jpeg', 'Patrick@gmail.com', 'Placquer', null),
-  (4, 'Abbie', 'Schmabbie', '10000.jpeg', 'Abbie@gmail.com', 'Aschabbie', null),
-  (5, 'David', 'Agarwal', '170000.jpeg', 'David@gmail.com', 'Dagarwal', null),
-  (6, 'Susie', 'Chen', '60000.jpeg', 'Susie@gmail.com', 'Schen', null),
-  (7, 'Matt', 'Gvido', '14780000.jpeg', 'Matt@gmail.com', 'Mvido', null),
-  (8, 'Zuirch', 'Hernández', '1780320948.jpeg', 'Zurich@gmail.com', 'Zhernandez',5),
-  (9, 'Will', 'Smith', '17800000089.jpeg', 'Will@gmail.com', 'Wsmith',4),
-  (10, 'Yuri', 'Mikhailov', '17800000293908.jpeg', 'Yuri@gmail.com', 'Ymikhailov',3)
-  ;
-=======
--- INSERT INTO accounts VALUES
---   (1, 'John', 'Smith', '1780000.jpeg', 'John@gmail.com', 'Jsmith', null),
---   (2, 'Dave', 'Jones', '178030988.jpeg', 'Dave@gmail.com', 'Djones', null),
---   (3, 'Patrick', 'Lacquer', '17800.jpeg', 'Patrick@gmail.com', 'Placquer', null),
---   (4, 'Abbie', 'Schmabbie', '10000.jpeg', 'Abbie@gmail.com', 'Aschabbie', null),
---   (5, 'David', 'Agarwal', '170000.jpeg', 'David@gmail.com', 'Dagarwal', null),
---   (6, 'Susie', 'Chen', '60000.jpeg', 'Susie@gmail.com', 'Schen', null),
---   (7, 'Matt', 'Gvido', '14780000.jpeg', 'Matt@gmail.com', 'Mvido', null),
---   (8, 'Zuirch', 'Hernández', '1780320948.jpeg', 'Zurich@gmail.com', 'Zhernandez',5),
---   (9, 'Will', 'Smith', '17800000089.jpeg', 'Will@gmail.com', 'Wsmith',4),
---   (10, 'Yuri', 'Mikhailov', '17800000293908.jpeg', 'Yuri@gmail.com', 'Ymikhailov',3)
---   ;
-
-
--- INSERT INTO accounts VALUES
---   ('John', 'Smith', '1780000.jpeg', 'John@gmail.com', 'Jsmith', null),
---   ('Dave', 'Jones', '178030988.jpeg', 'Dave@gmail.com', 'Djones', null),
---   ('Patrick', 'Lacquer', '17800.jpeg', 'Patrick@gmail.com', 'Placquer', null),
---   ('Abbie', 'Schmabbie', '10000.jpeg', 'Abbie@gmail.com', 'Aschabbie', null),
---   ('David', 'Agarwal', '170000.jpeg', 'David@gmail.com', 'Dagarwal', null),
---   ('Susie', 'Chen', '60000.jpeg', 'Susie@gmail.com', 'Schen', null),
---   ('Matt', 'Gvido', '14780000.jpeg', 'Matt@gmail.com', 'Mvido', null),
---   ('Zuirch', 'Hernández', '1780320948.jpeg', 'Zurich@gmail.com', 'Zhernandez',5),
---   ('Will', 'Smith', '17800000089.jpeg', 'Will@gmail.com', 'Wsmith',4),
---   ('Yuri', 'Mikhailov', '17800000293908.jpeg', 'Yuri@gmail.com', 'Ymikhailov',3)
---   ;
->>>>>>> 9c8829b6df4856683517c945104a14afe1b40ad2
---
+++ journals/Graham.md
@@ -0,0 +1,32 @@
+## Jan. 3, 2023 ##
+-> Team discussion
+-> issue/feature development
+
+## Jan. 4, 2023 ##
+-> Began setup of project repo
+-> troubleshooting errors in ghi
+-> app design discussions
+
+## Jan. 5, 2023 ##
+-> account_service created
+-> sql DB created for project app
+-> initial testing of accounts CRUD
+
+## Jan. 6, 2023 ##
+-> finalization of account_service CRUD
+-> synchronization/merge of project data from branches
+
+## Jan. 7, 2023 ##
+-> short look at ghi issues, non-resolved
+
+## Jan. 8, 2023 ##
+-> continued work at ghi, initial look into 3rd party apis
+
+## Jan. 9, 2023 ##
+-> ghi running and stable
+-> continued research on 3rd party apis (weather, google map api,)
+-> research into 3rd party api callbacks, needs continued research, abandoned for more critical features
+
+## Jan. 10, 2023 ##
+-> initial work into polling service
+-> creation of poll and option models
