a161e66fc34a3b3eaded2aab4a1d3a4d1caf9dfa
Steven <Stevenhuytran03@gmail.com>
2023-01-24 17:12:35 -0800

    Integration test for polls-service #05 (refactored schema imports)

--- polls_service/main.py
+++ polls_service/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Response, Depends
 from fastapi.middleware.cors import CORSMiddleware
 import os
-import schema
+from poll_schema import QuestionInfo, Question, QuestionEdit, QuestionCreate, ChoiceCreate, ChoiceList, ChoiceBase, QuestionBase
 from typing import List
 from sqlalchemy.orm import Session
 
@@ -31,41 +31,41 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/questions/", response_model=schema.QuestionInfo)
-def create_question(question: schema.QuestionCreate, db: Session = Depends(get_db)):
-	return crud.create_question(db=db, question=question)
+@app.post("/questions/", response_model=QuestionInfo)
+def create_question(question: QuestionCreate, db: Session = Depends(get_db)):
+    return crud.create_question(db=db, question=question)
 
-@app.get("/questions/", response_model=List[schema.Question])
+@app.get("/questions/", response_model=List[Question])
 def get_questions(db: Session = Depends(get_db)):
-	return crud.get_all_questions(db=db)
+    return crud.get_all_questions(db=db)
 
 def get_question_obj(db, qid):
-	obj = crud.get_question(db=db, qid=qid)
-	if obj is None:
-		raise HTTPException(status_code=404, detail="Question not found")
-	return obj
+    obj = crud.get_question(db=db, qid=qid)
+    if obj is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return obj
 
-@app.get("/questions/{qid}", response_model=schema.QuestionInfo)
+@app.get("/questions/{qid}", response_model=QuestionInfo)
 def get_question(qid: int, db: Session = Depends(get_db)):
-	return get_question_obj(db=db, qid=qid)
+    return get_question_obj(db=db, qid=qid)
 
-@app.put("/questions/{qid}", response_model=schema.QuestionInfo)
-def edit_question(qid: int, question: schema.QuestionEdit, db: Session = Depends(get_db)):
-	get_question_obj(db=db, qid=qid)
-	obj = crud.edit_question(db=db, qid=qid, question=question)
-	return obj
+@app.put("/questions/{qid}", response_model=QuestionInfo)
+def edit_question(qid: int, question: QuestionEdit, db: Session = Depends(get_db)):
+    get_question_obj(db=db, qid=qid)
+    obj = crud.edit_question(db=db, qid=qid, question=question)
+    return obj
 
 @app.delete("/questions/{qid}")
 def delete_question(qid: int, db: Session = Depends(get_db)):
-	get_question_obj(db=db, qid=qid)
-	crud.delete_question(db=db, qid=qid)
-	return {"detail": "Question deleted", "status_code": 204}
+    get_question_obj(db=db, qid=qid)
+    crud.delete_question(db=db, qid=qid)
+    return {"detail": "Question deleted", "status_code": 204}
 
-@app.post("/questions/{qid}/choice", response_model=schema.ChoiceList)
-def create_choice(qid: int, choice: schema.ChoiceCreate, db: Session = Depends(get_db)):
-	get_question_obj(db=db, qid=qid)
-	return crud.create_choice(db=db, qid=qid, choice=choice)
+@app.post("/questions/{qid}/choice", response_model=ChoiceList)
+def create_choice(qid: int, choice: ChoiceCreate, db: Session = Depends(get_db)):
+    get_question_obj(db=db, qid=qid)
+    return crud.create_choice(db=db, qid=qid, choice=choice)
 
-@app.put("/choices/{choice_id}/vote", response_model=schema.ChoiceList)
+@app.put("/choices/{choice_id}/vote", response_model=ChoiceList)
 def update_vote(choice_id: int, db: Session = Depends(get_db)):
-	return crud.update_vote(choice_id=choice_id, db=db)
+    return crud.update_vote(choice_id=choice_id, db=db)
