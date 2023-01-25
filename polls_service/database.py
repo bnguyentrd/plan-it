a0bbd0f0f99ab9a03b405b4c5efee7eadcaf72bd
Steven <Stevenhuytran03@gmail.com>
2023-01-24 20:03:57 -0800

    updated sqlalchemy database url

--- polls_service/database.py
+++ polls_service/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://polls:password@postgres/polls"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+SQLALCHEMY_DATABASE_URL = (
+    "postgres://postgres:5f16cd70ae6ab2c1@srv-captain--postgres-polls/postgres"
 )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
