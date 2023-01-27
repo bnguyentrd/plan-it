18f3a0eee6812de5591231fda461e985076aef2e
Steven <Stevenhuytran03@gmail.com>
2023-01-27 15:46:01 -0800

    testing noqa E501

--- polls_service/database.py
+++ polls_service/database.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 # longer one is for deployment, shorter for local
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:5f16cd70ae6ab2c1@srv-captain--postgres-polls/postgres"  # fmt: skip
-)
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:5f16cd70ae6ab2c1@srv-captain--postgres-polls/postgres"  # noqa: E501
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
