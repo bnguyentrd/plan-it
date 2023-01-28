3228fb293a81d94b2b62a89190f5d95ecd0fce14
Steven <Stevenhuytran03@gmail.com>
2023-01-27 19:05:56 -0800

    removed a test in accounts

--- accounts_service/tests/test_get_account.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from routers.accounts import AccountQueries, AccountOut
-from jwtdown_fastapi.authentication import Token
-from routers.accounts import get_authenticator
-
-
-# TEST WRITTEN BY STEVEN TRAN
